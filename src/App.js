0b3c2da86fd9cb9fa9df0107547de522f2b3af8f
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2022-02-13 13:01:49 +0100

    new features
    added custom console logs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,27 @@ import Contacts from './components/Contacts';
 import './App.css';
 
 function App() {
+  const styles =
+    'font-size: 1.25em; font-weight: bolder; padding:10px; background-color: rgba(0,0,0,0.8); color: white; border-radius:10px';
+  console.log(
+    `%c👨‍💻Hi, Im Vasyl Lespuch and Thanks for checking out my portfolio!`,
+    styles
+  );
+  const styles2 =
+    'font-size: 1.25em; font-weight: bolder; padding:10px; background-color: #90E0EF; border-radius:10px; opacity: 0;';
+  console.log('%c🖥️My GitHub Page: https://github.com/lespuch-v', styles2);
+  const styles3 =
+    'font-size: 1.25em; font-weight: bolder; padding:10px; background-color: #FFBBBB; border-radius:10px; opacity: 0;';
+  console.log(
+    '%c😅You can contact my by Facebook: https://www.facebook.com/vasyl.lespuch.3/',
+    styles3
+  );
+  const styles4 = `font-size: 1.25em; font-weight: bolder; padding:10px; background-color: #8BDB81; border-radius:10px;`;
+  console.log(
+    '%c😯Check out my Instagram page: https://www.instagram.com/vlcoding/?hl=en',
+    styles4
+  );
+
   return (
     <div className="App">
       <Header />
